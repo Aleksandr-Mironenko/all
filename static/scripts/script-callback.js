e11f973c5c4d7a4980e4c9134222bf92ed234204
Aleksandr-Mironenko <154741410+Aleksandr-Mironenko@users.noreply.github.com>
2024-04-21 22:32:16 +0300

    Add files via upload

---
+++ static/scripts/script-callback.js
@@ -0,0 +1,52 @@
+let pushCallbackLeftContainerChat = document.querySelector(".left__call");
+let pushCallbackHeaderChat = document.querySelector(
+  ".container-header__hidden-call"
+);
+let addCallback = document.querySelector(".callback");
+let pushClosedCallback = document.querySelector(".callback__header-button");
+let pushDocumentCallback = document.querySelector(".body-container");
+let leftDocumentCallback = document.querySelector(".left");
+
+let clickedCallback = false;
+
+function showCallback() {
+  //открывает блок Callback и скрывает оставшиеся блоки в зависимости от открытого левого контейнера
+  addCallback.classList.remove("callback-hidden");
+
+  if (clicked) {
+    leftDocumentCallback.classList.add("opacity-left");
+  } else {
+    pushDocumentCallback.classList.add("opacity");
+  }
+  clickedCallback = true;
+}
+
+function hideCallback() {
+  //скрытие блока Callback и открывает оставшиеся блоки в зависимости от открытого левого контейнера
+  addCallback.classList.add("callback-hidden");
+
+  if (clicked) {
+    leftDocumentCallback.classList.remove("opacity-left");
+  } else {
+    pushDocumentCallback.classList.remove("opacity");
+  }
+  clickedCallback = false;
+}
+
+function bodyClickCallback(event) {
+  //
+  if (
+    clickedCallback &&
+    !addCallback.contains(event.target) &&
+    !pushCallbackHeaderChat.contains(event.target) &&
+    !pushCallbackLeftContainerChat.contains(event.target)
+  ) {
+    hideCallback();
+  }
+}
+
+pushCallbackHeaderChat.addEventListener("click", showCallback);
+pushCallbackLeftContainerChat.addEventListener("click", showCallback);
+pushClosedCallback.addEventListener("click", hideCallback);
+pushDocumentCallback.addEventListener("click", bodyClickCallback);
+leftDocumentCallback.addEventListener("click", bodyClickCallback);
